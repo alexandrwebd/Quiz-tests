44d57f7a075edf14c50e28d265716fb26443e526
Alexandr <alexandrwebd@gmail.com>
2021-01-03 20:37:00 +0200

    add routing to project

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -96,6 +96,9 @@ class Quiz extends Component {
       results: {},
     })
   }
+  // componentDidMount() {
+  //   console.log('Quiz id', this.props.match.params.id)
+  // }
   render() {
     return (
       <div className={classes.Quiz}>
